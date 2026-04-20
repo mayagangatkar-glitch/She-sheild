556f85c4d00476e351451a749f800e6633e13d66
mayagangatkar-glitch <mayagangatkar@gmail.com>
2026-04-20 15:24:55 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,22 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/send-alert', methods=['POST'])
-def send_alert():
-    data = request.json
-    location = data.get('location')
-    contact = data.get('contact')
-
-    print("🚨 ALERT SENT")
-    print("📞 Contact:", contact)
-    print("📍 Location:", location)
-
-    return {"status": "ok"}
 
 if __name__ == "__main__":
     app.run()
